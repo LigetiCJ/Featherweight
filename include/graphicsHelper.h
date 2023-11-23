2c28bee8f6f949fc23f0c2e40655660c56c4c8f6
LigetiCJ <45637466+LigetiCJ@users.noreply.github.com>
2023-11-23 02:23:39 -0500

    half implemented openGL buffer abstraction

--- include/graphicsHelper.h
+++ include/graphicsHelper.h
@@ -7,8 +7,18 @@ typedef struct{
 	int *faces;
 }NOTE_model;
 
+typedef struct{
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+	unsigned int TEX;
+
+}NOTE_bufferObjects;
+
 GLFWwindow* initGraphics();
 unsigned int makeShaderProgram(const char *vertexShaderFilename, const char* fragmentShaderFilename);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void createBufferObjects(NOTE_bufferObjects *container, float *verts, int *faces );
+
 
 #endif
