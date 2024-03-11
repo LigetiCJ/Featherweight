b14d5a80e232804506b0f2f7b7f4354b1204e105
Noteclip <45637466+LigetiCJ@users.noreply.github.com>
2024-03-11 18:40:18 -0400

    there was a lot

---
+++ build/CMakeFiles/featherweight.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/featherweight.dir/main.c.o"
+  "CMakeFiles/featherweight.dir/main.c.o.d"
+  "featherweight"
+  "featherweight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/featherweight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/featherweight.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for featherweight.
+# This may be replaced when dependencies are built.
